3a74f6b1d1de3b6a699fd2dfd608196b03c87c1f
Geraud Tourrilhes <geraud.tourrilhes@gmail.com>
2018-06-01 10:27:36 +0200

    get departemnt

---
+++ src/main/java/dev/controller/CollaborateurController.java
@@ -0,0 +1,16 @@
+package dev.controller;
+
+public class CollaborateurController {
+
+	/*
+	 * GET /api/collaborateurs : retourne la liste des collaborateurs au format
+	 * JSON. GET /api/collaborateurs?departement=[ID_DEPARTEMENT]: retourne la
+	 * liste des collaborateurs au format JSON dont le département a
+	 * l'identifiant ID_DEPARTEMENT. GET /api/collaborateurs/[MATRICULE] :
+	 * retourne les informations d'un collaborateur. PUT
+	 * /api/collaborateurs/[MATRICULE] : modifie un collaborateur (données
+	 * envoyées au format JSON). GET /api/collaborateurs/[MATRICULE]/banque :
+	 * récupère les coordonnées bancaires d'un collaborateur (nom de la banque,
+	 * iban et bic) PUT /api/collaborateurs/[MATRICULE]/banque : mo
+	 */
+}
---
+++ src/main/java/dev/repository/CollaborateurRepository.java
@@ -0,0 +1,9 @@
+package dev.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import dev.entite.Collaborateur;
+
+public interface CollaborateurRepository extends JpaRepository<Collaborateur, Integer> {
+
+}
