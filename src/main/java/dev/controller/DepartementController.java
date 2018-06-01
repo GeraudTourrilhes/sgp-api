3188be07d987a814f7b7c6a25a780cd424cda5dc
Geraud Tourrilhes <geraud.tourrilhes@gmail.com>
2018-06-01 15:41:32 +0200

    cross origin

--- src/main/java/dev/controller/DepartementController.java
+++ src/main/java/dev/controller/DepartementController.java
@@ -3,6 +3,7 @@ package dev.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import dev.entite.Departement;
 import dev.repository.DepartementRepository;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api/departements")
 public class DepartementController {
 
